7f2e723dcccc7d7b71e8a219dfdcaa1750c1ccf3
LennDG <lenn.de.graef@gmail.com>
2023-12-05 09:56:30 +0100

    Finished part 2 day 3

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,9 +1,12 @@
+use std::collections::{HashMap, HashSet};
+
 mod parser;
 
 fn main() {
     let input = include_str!("input");
     let engine = parser::parse_engine(input);
-    part_one(engine)
+    //part_one(engine.clone());
+    part_two(engine);
 }
 
 fn part_one(engine: Engine) {
@@ -18,14 +21,64 @@ fn part_one(engine: Engine) {
                         check_adjacent(&mut integer, &engine, i, j)
                     }
                 }
-                SchematicData::Symbol(_) => sum += integer.flush(),
-                SchematicData::Gear => sum += integer.flush(),
-                SchematicData::Nothing => sum += integer.flush(),
+                SchematicData::Symbol(_) => {
+                    sum += integer.get_value();
+                    integer.flush()
+                }
+                SchematicData::Gear => {
+                    sum += integer.get_value();
+                    integer.flush()
+                }
+                SchematicData::Nothing => {
+                    sum += integer.get_value();
+                    integer.flush()
+                }
             }
         }
     }
 
     println!("The sum of all parts is: {sum}");
+}
+
+fn part_two(engine: Engine) {
+    let mut integer = Integer::new();
+    for (i, engine_line) in engine.schematics.iter().enumerate() {
+        for (j, schematic_data) in engine_line.iter().enumerate() {
+            match schematic_data {
+                SchematicData::Digit(value) => {
+                    integer.add_digit(*value);
+                    if !integer.adjacent {
+                        find_gears(&mut integer, &engine, i, j)
+                    }
+                }
+                SchematicData::Symbol(_) => integer.flush(),
+                SchematicData::Gear => integer.flush(),
+                SchematicData::Nothing => integer.flush(),
+            }
+        }
+    }
+
+    let mut sum = 0;
+    for adj in integer.gears.values() {
+        if adj.len() == 2 {
+            sum += adj[0] * adj[1];
+        }
+    }
+
+    println!("The sum of all gear ratios is: {sum}");
+}
+
+fn find_gears(integer: &mut Integer, engine: &Engine, i: usize, j: usize) {
+    println!("Center: {i}, {j}");
+
+    for y in i - 1..=i + 1 {
+        for x in j - 1..=j + 1 {
+            println!("Checked: {y}, {x}");
+            if let SchematicData::Gear = &engine.schematics[y][x] {
+                integer.add_adjacent_gear(y, x)
+            }
+        }
+    }
 }
 
 fn check_adjacent(integer: &mut Integer, engine: &Engine, i: usize, j: usize) {
@@ -51,6 +104,8 @@ fn check_adjacent(integer: &mut Integer, engine: &Engine, i: usize, j: usize) {
 struct Integer {
     buffer: Vec<u32>,
     adjacent: bool,
+    adjacent_gears: HashSet<usize>,
+    gears: HashMap<usize, Vec<u32>>,
 }
 
 impl Integer {
@@ -58,6 +113,8 @@ impl Integer {
         Integer {
             buffer: vec![],
             adjacent: false,
+            adjacent_gears: HashSet::new(),
+            gears: HashMap::new(),
         }
     }
 
@@ -76,19 +133,40 @@ impl Integer {
         value
     }
 
-    fn flush(&mut self) -> u32 {
-        let mut value = 0;
-        if self.adjacent {
-            value = self.get_value();
-        }
+    fn add_adjacent_gear(&mut self, i: usize, j: usize) {
+        let key = format!("{i}{j}").parse::<usize>().unwrap();
+        self.adjacent_gears.insert(key);
+
+        println!("{:?}", self.adjacent_gears)
+    }
+
+    fn flush(&mut self) {
+        self.flush_to_gears();
+
         self.buffer = vec![];
         self.adjacent = false;
+        self.adjacent_gears = HashSet::new();
+    }
 
-        value
+    fn flush_to_gears(&mut self) {
+        let v = self.get_value();
+
+        for key in &self.adjacent_gears {
+            match self.gears.get(key) {
+                Some(entry) => {
+                    let mut new_entry = entry.clone();
+                    new_entry.push(v);
+                    self.gears.insert(*key, new_entry);
+                }
+                None => {
+                    self.gears.insert(*key, vec![v]);
+                }
+            };
+        }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Engine {
     schematics: Vec<Vec<SchematicData>>,
 }
@@ -110,6 +188,8 @@ mod tests {
         let int = Integer {
             buffer: vec![4, 5, 8],
             adjacent: false,
+            gears: HashMap::new(),
+            adjacent_gears: HashSet::new(),
         };
         assert_eq!(int.get_value(), 458);
     }

7eadde7429544f3c15897cf5515da95edc9792c8
Lennart De Graef <lennart.degraef@external.atlascopco.com>
2023-12-12 17:11:28 +0100

    contiguous groups are correctly checked

--- 2023/day12/src/main.rs
+++ 2023/day12/src/main.rs
@@ -3,7 +3,7 @@ use color_eyre::eyre::Ok;
 mod parser;
 
 fn main() {
-    let input = include_str!("example");
+    let input = include_str!("test_check_finished");
     let condition_reports = parser::parse_condition_reports(input);
     part_one(condition_reports)
 }
@@ -11,7 +11,7 @@ fn main() {
 fn part_one(condition_reports: Vec<ConditionReport>) {}
 
 // Checks up to the first Unknown if the conditions match the groups
-fn check_finished(report: &ConditionReport) -> bool {
+fn check_contiguous_groups(report: &ConditionReport) -> bool {
     let finished: Vec<Condition> = report
         .conditions
         .iter()
@@ -19,7 +19,26 @@ fn check_finished(report: &ConditionReport) -> bool {
         .cloned()
         .collect();
 
-    false
+    let (mut groups, last): (Vec<u64>, u64) = finished.iter().fold(
+        (Vec::new(), 0),
+        |(mut groups, mut current_group), condition| {
+            match condition {
+                Condition::Operational => {
+                    groups.push(current_group);
+                    current_group = 0;
+                }
+                Condition::Damaged => current_group += 1,
+                Condition::Unknown => (),
+            }
+
+            (groups, current_group)
+        },
+    );
+    if last > 0 {
+        groups.push(last)
+    }
+
+    groups.iter().zip(report.groups.iter()).all(|(a, b)| a == b)
 }
 
 #[derive(Debug, Clone)]
